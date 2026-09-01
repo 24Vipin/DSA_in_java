e477740262c3a27077e1901998e2855e3f05d0b3
Vipin Yadav <sy7556654@gmail.com>
2026-09-01 16:31:54 +0530

    Add files via upload

---
+++ In_Num_indx.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class In_Num_indx {
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int[] arr={10, 20,30,40,50};
+    int n=arr.length;
+    for(int i=0; i<n; i++){
+        System.out.println(arr[i]);
+    }
+    System.out.println("Enter index no.: ");
+    int x=sc.nextInt();
+
+    System.out.println("Enter element: ");
+    int y=sc.nextInt();
+
+    int[] arr1=new int[n+1];
+
+    for(int i=0; i<=arr1.length; i++ ){
+
+    if(x<i){
+        
+    }
+    }
+
+
+
+
+
+
+}
+}
---
+++ nCR.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class nCR {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+//        // This code is for nCr
+//        System.out.println("Enter a number n: ");
+//        int n=sc.nextInt();
+//
+//        System.out.println("Enter a number r: ");
+//        int r= sc.nextInt();
+//
+//        if(r>n){
+//            System.out.println("Invalid ! r cannot be greater than  n ");
+//        }
+//        else{
+//            int factN=1,  factR=1, factNR=1;
+//            for(int i=1; i<=n;i++) factN=factN*i;
+//            for(int i=1; i<=r; i++) factR=factR*i;
+//            for(int i=1; i<=(n-r); i++) factNR=factNR*i;
+//            System.out.println("nCR: "+ factN/factR*factNR);
+//        }
+//sc.close();
+
+        // Fibonacci
+//        System.out.println("Enter a number : ");
+//        int n=sc.nextInt();
+//
+//        int a=0;
+//        int b=1;
+//        System.out.println(b);
+//
+//        for(int i=1; i<=n; i++){
+//            int fibo=a+b;
+//            System.out.println(fibo);
+//
+//
+//            a=b;
+//            b=fibo;
+//
+//        }
+
+        //ArmStrong
+//        System.out.println("Enter a number: ");
+//        int n=sc.nextInt();
+//
+//        int original=n;
+//        double sum=0;
+//        int digit = String.valueOf(n).length();
+//        while(n>0){
+//            int rem=n%10;
+//            sum = sum + Math.pow(rem, digit);
+//             n=n/10;
+//        }
+//        if(original==sum){
+//            System.out.println(original + " is a Armstrong");
+//        }
+//        else{
+//            System.out.println(original +" is not a Armstrong");
+//        }
+
+//      Palindrome Number
+//        System.out.println("Enter a number: ");
+//        int n =sc.nextInt();
+//
+//        int original=n;
+//        int rev=0;
+//        while(n>0){
+//            int rem=n%10;
+//            rev=rev*10+rem;
+//            n=n/10;
+//        }
+//        if(original==rev){
+//            System.out.println(original + " is Palindrome");
+//        }
+//        else{
+//            System.out.println(original +" is not Palindrome");
+//        }
+
+
+
+
+    }
+}
